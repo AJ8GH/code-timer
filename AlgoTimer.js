3e29073b536eb26e9390db25f05fbbefc0fbe8ee
Adam Jonas <adamjonas25@gmail.com>
2021-05-21 17:25:58 +0100

    Update main.js

--- AlgoTimer.js
+++ AlgoTimer.js
@@ -1,3 +1,9 @@
+import AlgoRunner from './lib/AlgoRunner'
+import CodeTimer from './lib/CodeTimer'
+
 export function printMsg () {
   console.log('This is a message from the demo package')
 }
+
+export default AlgoRunner
+export { CodeTimer }
